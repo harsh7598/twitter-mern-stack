988e9350e9ef45bcd8548e2250ad43a4d4aa63ab
harsh7598 <harryht7@gmail.com>
2024-06-24 15:11:55 +0530

    project setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded)
+
+app.use("/api/auth",authRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+});
